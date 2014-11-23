7a886c5ef1ea7b49adc658dda02a2d3aae493644
newlifechou <newlifechou@gmail.com>
2014-11-23 23:39:11 +0800

    more

---
+++ MvcCDISCS/Scripts/js/home.js
@@ -0,0 +1,21 @@
+﻿$(function () {
+    //when mouse is over the category list ,change the background the li mark
+    $("#leftarea ul.categorylist li").mouseover(function () {
+        $(this).addClass("mo");
+    }).mouseout(function () {
+        $(this).removeClass("mo");
+    });
+
+    //same height of the leftarea and rightarea in the middle part
+    var h1 = $("div#leftarea ul.categorylist").height();
+    var h2 = $("div#about").height();
+    var h3;
+    if (h1>h2) {
+        h3 = h1-6;
+    }
+    else {
+        h3 = h2;
+    }
+
+    $("div#about").height(h3);
+});
