b19242ba24143e1ddce9e1b77866256faf7edfd2
newlifechou <newlifechou@gmail.com>
2014-11-24 09:07:32 +0800

    add productcategory class

---
+++ MvcCDISCS/Models/product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcCDISCS.Models
+{
+    /// <summary>
+    /// 产品详细信息
+    /// </summary>
+    public class product
+    {
+        public int ProductId { get; set; }
+        [Required(ErrorMessage = "不能为空")]
+        public string ProductName { get; set; }
+        [Required(ErrorMessage = "不能为空")]
+        public DateTime PublishTime { get; set; }
+        [Required(ErrorMessage = "不能为空")]
+        public int ReadCount { get; set; }
+        [Required(ErrorMessage = "不能为空")]
+        public string Photo { get; set; }
+        [Required(ErrorMessage = "不能为空")]
+        public int CategoryId { get; set; }
+        public string Content { get; set; }
+        public virtual productcategory Category { get; set; }
+    }
+}
---
+++ MvcCDISCS/Models/productcategory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcCDISCS.Models
+{
+    /// <summary>
+    /// productcategory
+    /// </summary>
+    public class productcategory
+    {
+        public int CategoryId { get; set; }
+        [Required(ErrorMessage = "不能为空")]
+        public string CategoryName { get; set; }
+        [Required(ErrorMessage = "不能为空")]
+        public string Photo { get; set; }
+        [Required(ErrorMessage = "不能为空")]
+        public string Memo { get; set; }
+    }
+}
