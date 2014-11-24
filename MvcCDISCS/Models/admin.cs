3d169049307f6e1af54a3f3ca5a04818534142ad
newlifechou <newlifechou@gmail.com>
2014-11-24 08:42:37 +0800

    nuget添加EF和json，添加admin类

---
+++ MvcCDISCS/Models/admin.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcCDISCS.Models
+{
+    /// <summary>
+    /// admin表-登录用
+    /// </summary>
+    public class admin
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage="用户名不能为空")]
+        [StringLength(50,ErrorMessage="用户名长度不能大于50")]
+        public string UserName { get; set; }
+        [Required(ErrorMessage = "密码不能为空")]
+        public string Password { get; set; }
+        [Required]
+        public string Role { get; set; }
+    }
+}
