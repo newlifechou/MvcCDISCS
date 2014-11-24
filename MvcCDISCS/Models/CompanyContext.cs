e333e62a20cc8ff704d7c2ed40659e2c08cb32df
newlifechou <newlifechou@gmail.com>
2014-11-24 09:40:08 +0800

    add class service and CompanyContext final

---
+++ MvcCDISCS/Models/CompanyContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MvcCDISCS.Models
+{
+    /// <summary>
+    /// CompanyContext
+    /// </summary>
+    public class CompanyContext : DbContext
+    {
+        //set all the dbset
+        public DbSet<admin> admins { get; set; }
+        public DbSet<basicinfo> basicinfos { get; set; }
+        public DbSet<morebasicinfo> morebasicinfos { get; set; }
+        public DbSet<flash> flashs { get; set; }
+        public DbSet<product> products { get; set; }
+        public DbSet<productcategory> productcategorys { get; set; }
+        public DbSet<service> services { get; set; }
+        public DbSet<news> newss { get; set; }
+        public DbSet<feedback> feedbacks { get; set; }
+        public DbSet<friendlink> friendlink { get; set; }
+        public DbSet<log> logs { get; set; }
+        public DbSet<career> careers { get; set; }
+        public DbSet<notice> notices { get; set; }
+
+    }
+}
---
+++ MvcCDISCS/Models/service.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcCDISCS.Models
+{
+    /// <summary>
+    /// service
+    /// </summary>
+    public class service
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "不能为空")]
+        public string Title { get; set; }
+        [Required(ErrorMessage = "不能为空")]
+        public string Photo { get; set; }
+        [Required(ErrorMessage = "不能为空")]
+        public string Content { get; set; }
+
+    }
+}
