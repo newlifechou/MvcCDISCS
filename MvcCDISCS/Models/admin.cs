811ecbfba4b4123eeba664b951b4469cde4135ba
newlifechou <newlifechou@gmail.com>
2014-11-24 08:47:51 +0800

    添加basicinfo类

--- MvcCDISCS/Models/admin.cs
+++ MvcCDISCS/Models/admin.cs
@@ -12,10 +12,10 @@ namespace MvcCDISCS.Models
     public class admin
     {
         public int Id { get; set; }
-        [Required(ErrorMessage="用户名不能为空")]
-        [StringLength(50,ErrorMessage="用户名长度不能大于50")]
+        [Required(ErrorMessage="不能为空")]
+        [StringLength(50,ErrorMessage="长度不能大于50")]
         public string UserName { get; set; }
-        [Required(ErrorMessage = "密码不能为空")]
+        [Required(ErrorMessage = "不能为空")]
         public string Password { get; set; }
         [Required]
         public string Role { get; set; }
---
+++ MvcCDISCS/Models/basicinfo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcCDISCS.Models
+{
+    public class basicinfo
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "不能为空")]
+        [StringLength(50, ErrorMessage = "长度不能大于50")]
+        public string CompanyName { get; set; }
+        [StringLength(50, ErrorMessage = "长度不能大于50")]
+        public string Contact { get; set; }
+        [StringLength(50, ErrorMessage = "长度不能大于80")]
+        public string Phone { get; set; }
+        [StringLength(50, ErrorMessage = "长度不能大于50")]
+        public string CellPhone { get; set; }
+        [StringLength(50, ErrorMessage = "长度不能大于50")]
+        public string Fax { get; set; }
+        [StringLength(50, ErrorMessage = "长度不能大于80")]
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public string PostCode { get; set; }
+    }
+}
