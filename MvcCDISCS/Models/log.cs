72e78d6dfac7bc2a65b6e2cb143b9dab55f3dadf
newlifechou <newlifechou@gmail.com>
2014-11-24 09:21:54 +0800

    add class log

---
+++ MvcCDISCS/Models/log.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcCDISCS.Models
+{
+    /// <summary>
+    /// log
+    /// </summary>
+    public class log
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string LogTime { get; set; }
+        public string IpAddress { get; set; }
+        public string Memo { get; set; }
+    }
+}
