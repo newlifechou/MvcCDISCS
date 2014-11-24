1059ad7c6086a9c3de164b5d43e053b500f04ddb
newlifechou <newlifechou@gmail.com>
2014-11-24 09:26:57 +0800

    add class notice,basically done.more update will be on

---
+++ MvcCDISCS/Models/notice.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcCDISCS.Models
+{
+    /// <summary>
+    /// notice公告
+    /// </summary>
+    public class notice
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "不能为空")]
+        public string NoticeContent { get; set; }
+        public DateTime CreateTime { get; set; }
+        public int Priority { get; set; }
+        public bool IsShow { get; set; }
+    }
+}
