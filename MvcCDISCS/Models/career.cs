8b4d768215dd383c75c0c77f67d211d756c8b33f
newlifechou <newlifechou@gmail.com>
2014-11-24 09:25:02 +0800

    add class career

---
+++ MvcCDISCS/Models/career.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcCDISCS.Models
+{
+    /// <summary>
+    /// career
+    /// </summary>
+    public class career
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "不能为空")]
+        public string Title { get; set; }
+        public DateTime CreateTime { get; set; }
+        public DateTime ExpirationTime { get; set; }
+        [Required(ErrorMessage = "不能为空")]
+        public string Content { get; set; }
+        public string Memo { get; set; }
+        public bool IsShow { get; set; }
+    }
+}
