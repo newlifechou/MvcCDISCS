1201199862149c507aac8ee100a26869607fa262
newlifechou <newlifechou@gmail.com>
2014-11-24 09:19:41 +0800

    add class friendlinnk

---
+++ MvcCDISCS/Models/friendlink.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcCDISCS.Models
+{
+    /// <summary>
+    /// friendlink 
+    /// </summary>
+    public class friendlink
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "不能为空")]
+        [StringLength(20, ErrorMessage = "长度不能大于20")]
+        public string SiteName { get; set; }
+        [Required(ErrorMessage = "不能为空")]
+        public string SiteUrl { get; set; }
+    }
+}
