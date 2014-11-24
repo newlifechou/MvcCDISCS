8d804c2520166641f1a7949b80dc631d37ea291d
newlifechou <newlifechou@gmail.com>
2014-11-24 09:10:56 +0800

    add news class

---
+++ MvcCDISCS/Models/news.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcCDISCS.Models
+{
+    /// <summary>
+    /// news
+    /// </summary>
+    public class news
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "不能为空")]
+        public string Title { get; set; }
+        [Required(ErrorMessage = "不能为空")]
+        public string Author { get; set; }
+        [Required(ErrorMessage = "不能为空")]
+        public DateTime PublishTime { get; set; }
+        [Required(ErrorMessage = "不能为空")]
+        public int ReadCount { get; set; }
+        public string Content { get; set; }
+    }
+}
