98c363d818c14fe2dffdf1a09b24e8f6612cea28
newlifechou <newlifechou@gmail.com>
2014-11-25 10:13:49 +0800

    改进js切换代码

--- MvcCDISCS/Scripts/js/home.js
+++ MvcCDISCS/Scripts/js/home.js
@@ -1,17 +1,26 @@
 ﻿$(function () {
     //when mouse is over the category list ,change the background the li mark
-    $("#leftarea ul.categorylist li").mouseover(function () {
-        $(this).addClass("mo");
-    }).mouseout(function () {
-        $(this).removeClass("mo");
-    });
+    //$("#leftarea ul.categorylist li").mouseover(function () {
+    //    $(this).addClass("mo");
+    //}).mouseout(function () {
+    //    $(this).removeClass("mo");
+    //});
+    $("#leftarea ul.categorylist li").hover(
+        function () {
+            $(this).addClass("mo");
+        },
+        function () {
+            $(this).removeClass("mo");
+        }
+    );
+
 
     //same height of the leftarea and rightarea in the middle part
     var h1 = $("div#leftarea ul.categorylist").height();
     var h2 = $("div#about").height();
     var h3;
-    if (h1>h2) {
-        h3 = h1-6;
+    if (h1 > h2) {
+        h3 = h1 - 6;
     }
     else {
         h3 = h2;
