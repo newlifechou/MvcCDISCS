840595c117c1e1d76f4d2a3199a7182c2b6b6c5a
newlifechou <newlifechou@gmail.com>
2014-11-28 15:40:52 +0800

    move dbmodels class into a directory,and add UserRepository

---
+++ MvcCDISCS/Models/UserRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+
+namespace MvcCDISCS.Models
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class UserRepository
+    {
+        CompanyContext db = new CompanyContext();
+        public bool ValidateUser(string userName, string passWord)
+        {
+            return db.admin.Any(o => o.UserName == userName && o.Password == passWord);
+        }
+    }
+}
