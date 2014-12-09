8c173f47b03935f8d9f7e4d40db16358437d2692
newlifechou <newlifechou@gmail.com>
2014-12-10 01:05:35 +0800

    js to realize the friendlink function

--- MvcCDISCS/Scripts/js/layout.js
+++ MvcCDISCS/Scripts/js/layout.js
@@ -1,7 +1,14 @@
 ﻿$(function () {
     //when click the menu nav ,change the style
-    $("#mainnav li a").click(function () {
-        $(this).addClass("selected");
-        $(this).parent().siblings().children("a").removeClass("selected");
+    //$("#mainnav li a").click(function () {
+    //    $(this).addClass("selected");
+    //    $(this).parent().siblings().children("a").removeClass("selected");
+    //});
+
+
+    //open friendlink
+    $("#btnFriendlinkGo").click(function () {
+        var flstr = $("#FriendLinkList").val();
+        window.open(flstr);
     });
 });
